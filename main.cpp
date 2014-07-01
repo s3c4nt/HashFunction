53a6ffdfcdc8062368fde9429571beeb82894864
s3c4nt <khipu@outlook.com>
2014-06-30 20:28:29 -0700

    cleaned main.cpp, FindMatch to be written seperately

--- main.cpp
+++ main.cpp
@@ -3,63 +3,40 @@
 #include <cstring>
 using namespace std;
 
-char Username[]= " ";
-char Password[]= " ";
+// moved FindMatch function to it's own file and call it in main as needed. 
 
-int FindMatch(Username[], Password[]);
 
 int main(){
-char option=' '; // hold user options for N (new user) and L (login)
+  char option=' '; // hold user options for N (new user) and L (login)
+  cout <<"Welcome to SlikHash Version .01 \n";
+  cout <<"Main Menu: \n";
+  cout <<" (N) New User \n (L) Login \n (Q) Quit \n your choice: ";
+  // future: move this main menu printing and selection (?) to it's own function and call it from main(). This will keep main cleaner and allow menu alteration without derping with main() directly. - DC
+  cin.get(option);  //read in user choice for new user creation or login
 
-cout <<" (N) New User \n (L) Login \n (Q) Quit \n your choice: ";
- 
-cin.get(option, 100, '\n');  //read in user choice for new user creation or login
-
-switch(toupper(option)){
- case 'N':              //New User option
+  switch(toupper(option)){
+  case 'N':              //New User option
    //   GetUserInput();
  
-  break;
+    break;
  
- case 'L':              //Login option
+  case 'L':              //Login option
    //   FindMatch();
  
-  break;
+    break;
 
- case 'Q':            //quit the program option
+  case 'Q':            //quit the program option
  
-  break;
+    break;
  
- default:
-   cout<< "Error: Please Choose Again:"; 
-};
+  default:
+    cout<< "Error: Invalid Choice. \n Please Choose Again. \n your choice: "; 
+  };
 
-
-
-
-//FindMatch will query the username/passwd file and check for existing
-//entries against what was passed to it
-
-int FindMatch(Username[], Password[]){
-
-
-ifstream infile;
-infile.open ("UserInfo.txt");
-
-
-
-
-while(!infile.eof){
-char tempusername[]= " ";
-char temppasswd[]= " ";
- infile.get(tempusername,100,';')
-
-  if (Username==tempusername){
-   return=1;}
-
-infile.ignore(temppasswd,100,'/n');
- 
 }
 
- return 0;
-};
+
+
+
+
+  
