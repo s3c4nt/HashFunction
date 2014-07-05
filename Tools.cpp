9d0d7b3f0583ce63b912d7d74b28dd2d661abdf4
s3c4nt <khipu@outlook.com>
2014-07-05 06:16:20 -0700

    It runs, test it

--- Tools.cpp
+++ Tools.cpp
@@ -7,13 +7,14 @@ const int MAX = 100;
 
 void menu()
 {
-    char option=' '; // hold user options for N (new user) and L (login)
-
+  char makeOptionUpperCase= ' ';  
+  char option=' '; // hold user options for N (new user) and L (login)
+    
     cout <<" (N) New User \n (L) Login \n (Q) Quit \n your choice: ";
 
-    cin >> option;  //read in user choice for new user creation or login
-
-    switch(toupper(option))
+    cin >> makeOptionUpperCase;  //read in user choice for new user creation or login
+    option = toupper(makeOptionUpperCase); //toupper does not change the parameter
+    switch(option)
     {
      case 'N':              //New User option
          getUsernamePassword();
@@ -34,38 +35,42 @@ void menu()
     }
 }
 
-void getUsernamePassword()
+int getUsernamePassword() 
 {
-	char username[MAX] = " ";
-	char password[MAX] = " ";
-
+  char username[MAX] = " ";
+  char password[MAX] = " ";
+  int outputValue;
 	cout << "Please enter your desired Username: ";
-	cin.get(username, 100, '\n');
-	cin.ignore(100, '\n');
+	cin>>username;
+	//	cin.ignore(100, '\n');
+	//we ask the user to pick a username here - they should type a username which the above cin.get takes in...why are we ignoring a second line of input that we did not ask for? this was most likely left over from when getUsernamePassword was used for more than user account creation. - s3c4nt
 
-	cout << endl;
+	cout << endl; //not sure why this is here, after they type a username they hit enter which should move it down by default.
 
 	cout << "Now enter your desired Password: ";
-	cin.get(password, 100, '\n');
-	cin.ignore(100, '\n');
+	cin>>password;
+	//	cin.ignore(100, '\n');
+	//again, we ask them for one line of text and use a cin.get to grab that but randomly have a cin.ignore to ignore the second line of text they were not prompted for and won't enter - s3c4nt
 
 	cout << "Checking for errors..." << endl;
 	
+	char *tempUname = username;
+	char *tempPass = password;
+	  outputValue = findMatch(*tempUname,*tempPass);
 
-
-	if(findMatch(username, password) == 1)
+	if(outputValue == 1) 
 	{
-		cout << "Username already exists. Login in again." << endl;
+		cout << "Username already exists. Please choose another." << endl;
 		getUsernamePassword();
 	}
 	else
 	{
-		cout << "we just simulated a hash." << endl;
+		cout << " hash function will output to file!." << endl;
 		//hashFunction();
 
 
+	}
 }
-
 
 //FindMatch will query the username/passwd file and check for existing
 //entries against what was passed to it
@@ -74,19 +79,22 @@ int findMatch(char &username, char &password)
 {
     ifstream infile;
     infile.open("UserInfo.txt");
-
+    char unameplaceholder[MAX] = " ";
+    unameplaceholder[MAX] = username;
+    char pwdplaceholder[MAX] = " ";
+    pwdplaceholder[MAX] = password;
     while(!infile.eof())
     {
-        char tempusername[100]= " ";
-        char temppasswd[100]= " ";
+        char tempusername[MAX]= " ";
+        char temppasswd[MAX]= " ";
         infile.get(tempusername, 100, ';');
 
-        if(strcmp(username, tempusername))
+        if(strncmp(unameplaceholder, tempusername,100) == 1)
         {
             return 1;
         }
 
-        infile.ignore(100, '\n');
+        infile.ignore(100, '\n'); //this ignores the pwds in UserInfo.txt
     }
     return 0;
 }
--- Tools.h
+++ Tools.h
@@ -1,12 +1,14 @@
 //Header file for all utilities used throughout the program
 #include "UsersClass.h"
 #include <iostream>
-using namespace std;
+#include <string.h>
+#include <stdio.h>
+using namespace std;  //stackoverflow says this is bad mojo in a header file
 #ifndef TOOLS_H
 #define TOOLS_H
 
 void menu();
-void getUsernamePassword();
+int getUsernamePassword();
 int findMatch(char &, char &);
 
 
