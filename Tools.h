7aab60b9eaa63582e86cd337b4952bd885ceb5fb
ksliker89 <kevin.sliker@gmail.com>
2014-08-04 15:36:42 -0700

    made getusername a void

--- Tools.h
+++ Tools.h
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
-using namespace std;  //stackoverflow says this is bad mojo in a header file
+
 #ifndef TOOLS_H
 #define TOOLS_H
 
 void menu();
-int getUsernamePassword();
+void getUsernamePassword();
 int findMatch(char &, char &);
 
 
